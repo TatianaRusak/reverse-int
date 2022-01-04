86ef044358d7b41fcfe219626720ce5b78bcc668
Tatiana Rusak <tatianarusak85@gmail.com>
2022-01-04 13:19:09 +0300

    feat: realize functio reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    // 'use strict';
+    n = (Math.abs(n)).toString();
+    let result = '';    
+    for (let i = 0; i < n.length; i++) {
+        result = `${n[i]}${result}`;
+    }
+    return result;
 }
